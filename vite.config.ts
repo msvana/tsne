84dd2a0642c47d21cb94ad9425903091032e66d4
Milos Svana <milos.svana@mailfence.com>
2025-04-02 10:09:07 +0200

    Add compiled demo

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,26 @@
 import { defineConfig } from "vite";
 
+const removeCrossOrigin = () => {
+    return {
+        name: "remove-crossorigin-attribute",
+        transformIndexHtml(html: string) {
+            return html
+                .replace(/crossorigin /g, "")
+                .replace('<script type="module" ', "<script defer ")
+                .replace("/assets", "assets");
+        },
+    };
+};
+
 export default defineConfig({
+    root: "src/",
     build: {
         rollupOptions: {
             input: {
                 main: "src/example.html",
             },
         },
+        outDir: "../demo/",
     },
+    plugins: [removeCrossOrigin()],
 });
